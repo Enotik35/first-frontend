20e0690c4d6d1cc2e970cb7ac2110b67e5185f62
Enotik <42745849+Enotik35@users.noreply.github.com>
2018-12-13 21:04:14 +0400

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function texting() {
-			let ipt = document.getElementById('input').value;
-			var textElem = document.createElement('div');
-			textElem.className = "msg "
-			textElem.innerHTML = ipt;
-			document.body.appendChild(textElem);
-		}
-		
