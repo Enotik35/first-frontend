9b63ecfe8627bf9b26fefa475b05da457f87187e
Enotik <42745849+Enotik35@users.noreply.github.com>
2018-12-13 21:16:48 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,18 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer(function (request,response) {
-console.log(request.method, request.url);
-if (request.url === '/style.css') {
-const css = fs.readFileSync('style.css','utf8');
-response.end(css);
-} else {
-const text = fs.readFileSync('index.html','utf8');
-response.end(text);
-}
+const server = http.createServer(function (request, response) {
+	console.log(request.method, request.url);
+	if (request.url == '/style.css') {
+		const css = fs.readFileSync('style.css', 'utf8');
+		response.end(css);
+	} else {
+		const html = fs.readFileSync('index.html', 'utf8');
+		response.end(html);
+	}
 });
-console.log('port = ',proccess.env.PORT);
-server.listen(proccess.env.PORT);
+
+console.log('port = ', process.env.PORT);
+
+server.listen(process.env.PORT || 3000);
+console.log('Server started!');
